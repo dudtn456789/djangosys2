2420b34f34f14434f81d46659c93b4b2933fca59
dudtn456789 <dudtn456789@naver.com>
2025-05-07 13:39:40 +0900

    7주차

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,11 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 
 # Create your tests here.
+class TestView(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def test_post_list(self):
+        
+
+
